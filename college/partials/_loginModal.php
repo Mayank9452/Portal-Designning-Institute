d93d9a1ef837badc680686b4a1d15f43af303994
Kaushik Dutta <kaushik.dutta_cs18@gla.ac.in>
2020-11-20 12:56:25 +0530

    login modal added

---
+++ college/partials/_loginModal.php
@@ -0,0 +1,37 @@
+
+
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleloginLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="loginModalLabel">Login to College Choice</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+
+      <form action="/Portal-Designning-Institute/college/partials/_handleLogin.php" method="POST">
+      <div class="modal-body">
+              
+            <div class="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input type="text" class="form-control" id="loginEmail" name="loginEmail" aria-describedby="emailHelp">
+                <!--<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="loginPassword" name="loginPassword">
+            </div>
+         
+            <button type="submit" class="btn btn-primary">Login</button>
+            
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+      </form>
+
+    </div>
+  </div>
+</div>
